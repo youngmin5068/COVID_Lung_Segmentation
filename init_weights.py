8205db30cc33a9909818f827be0a3c1746ce2cfd
youngmin5068 <61230321+youngmin5068@users.noreply.github.com>
2022-12-29 14:15:56 +0900

    weight initialization

---
+++ init_weights.py
@@ -0,0 +1,12 @@
+import torch
+import torch.nn as nn
+from torch.nn import init
+
+class m():
+    print("")
+
+def weights_init_normal(m):
+    classname = m.__class__.__name__
+    print(classname)
+
+weights_init_normal(m)
