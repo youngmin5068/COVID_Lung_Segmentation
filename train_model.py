b956b685a8fc7a4b9b13724c7d6ee87d2131894c
keemym <youngmin980118@gmail.com>
2023-03-02 14:28:05 +0900

    model training with evaluation

--- train_model.py
+++ train_model.py
@@ -1,12 +1,11 @@
-import argparse
 import logging
 import os
-import random
 import torch
 import torch.optim as optim
 from torch.utils.data import DataLoader
 import torch.nn as nn
 import time
+from eval import eval_net
 from custom_transforms import *
 from network import *
 from dataset import *
@@ -18,6 +17,7 @@ from dice_loss import *
 Model_SEED = 76075154
 
 dataset_path = "/workspace/test_visual2"
+eval_path = "/workspace/eval"
 
 def train_net(net,
               device,
@@ -29,9 +29,10 @@ def train_net(net,
               ):
 
     dataset = seg_Dataset(dataset_path)
+    eval_dataset = seg_Dataset(eval_path)
     n_train = len(dataset)
     train_loader = DataLoader(dataset, batch_size=batch_size,shuffle=True)
-
+    val_loader = DataLoader(eval_dataset,batch_size=batch_size,shuffle=True)
 
     logging.info(f'''Starting training:
         Epochs:          {epochs}
@@ -49,12 +50,12 @@ def train_net(net,
         criterion = nn.BCEWithLogitsLoss() 
 
     for epoch in range(epochs):
+        eval_count = 0
         start = time.time()
         net.train()
         i=1
-        scheduler = optim.lr_scheduler.MultiStepLR(optimizer, milestones=[1000,3000,5000,7000,10000,15000], gamma=2)
-
-        for e, (imgs,true_masks) in enumerate(train_loader):
+    
+        for imgs,true_masks in train_loader:
 
             imgs = imgs.to(device=device,dtype=torch.float32)
             true_masks = true_masks.to(device=device,dtype=torch.float32)
@@ -73,16 +74,19 @@ def train_net(net,
             loss.backward()
             nn.utils.clip_grad_value_(net.parameters(), 0.1)
             optimizer.step()
-            #scheduler.step()
-
-            #print('epoch : {}, index : {}/{}, loss (batch) : {:.4f}'.format(epoch+1, e,n_train,loss.item()))
 
             if i*batch_size%50 == 0:
                 print('epoch : {}, index : {}/{}, loss (batch) : {:.4f}'.format(epoch+1, i*batch_size,n_train,loss.item())) 
 
             i += 1
 
-        dir_checkpoint = '/workspace/dir_checkpoint'
+            if i*batch_size%500 == 0:
+                eval_count += 1
+                val_score = eval_net(net, val_loader, device,deep_supervision=False)
+                print('Validation Dice Coeff : {:.4f}'.format(val_score))
+
+
+        dir_checkpoint = '/workspace/dir_checkpoint_0302'
 
         if save_cp:
             try:
@@ -90,25 +94,11 @@ def train_net(net,
                 logging.info("Created checkpoint directory")
             except OSError:
                 pass
-            torch.save(net.state_dict(), dir_checkpoint + f'/CP_epoch_next_{epoch+1}.pth')
+            torch.save(net.state_dict(), dir_checkpoint + f'/CP_epoch_0302_{epoch+1}.pth')
             logging.info(f'Checkpoint {epoch + 1} saved !')
         end = time.time()
         print('Epoch time : ' + str((end - start) // 60))
 
-
-def get_args():
-    parser = argparse.ArgumentParser(description='Train the UNet on images and target masks',
-                                     formatter_class=argparse.ArgumentDefaultsHelpFormatter)
-    parser.add_argument('-e', '--epochs', metavar='E', type=int, default=200,
-                        help='Number of epochs', dest='epochs')
-    parser.add_argument('-b', '--batch-size', metavar='B', type=int, nargs='?', default=8,
-                        help='Batch size', dest='batchsize')
-    parser.add_argument('-l', '--learning-rate', metavar='LR', type=float, nargs='?', default=0.0001,
-                        help='Learning rate', dest='lr')
-    parser.add_argument('-f', '--load', dest='load', type=str, default=False,
-                        help='Load model from a .pth file')
-
-    return parser.parse_args()
 
 if __name__ == '__main__':
     torch.manual_seed(Model_SEED)
@@ -116,7 +106,6 @@ if __name__ == '__main__':
     torch.backends.cudnn.benchmark = False
 
     logging.basicConfig(level=logging.INFO, format='%(levelname)s: %(message)s')
-    args = get_args()
 
     device = torch.device(f'cuda' if torch.cuda.is_available() else 'cpu')
 
@@ -129,11 +118,6 @@ if __name__ == '__main__':
     #net = UNet_3Plus(input_channels=1,num_classes=1)
     #net = UNet_3Plus_DeepSup_CGM(input_channels=1,num_classes=1)
 
-    if args.load:
-        net.load_state_dict(
-            torch.load(args.load, map_location=device)
-        )
-        logging.info(f'Model loaded from {args.load}')
 
     # if torch.cuda.device_count() > 1:
     #     net = nn.DataParallel(net)
@@ -141,11 +125,3 @@ if __name__ == '__main__':
     net.to(device)
 
     train_net(net=net,device=device)
-
-    
-    # train_net(net=net,
-    #               epochs=args.epochs,
-    #               batch_size=args.batchsize,
-    #               lr=args.lr,
-    #               device=device,
-    #               deep_supervision=False)
